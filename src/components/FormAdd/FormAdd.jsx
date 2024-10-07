abc322660aa892ab2cea81bcaca6cec45520d4df
casetal <ax.bratsun@gmail.com>
2024-10-07 23:27:37 +0500

    простой калькулятор калорий

--- src/components/FormAdd/FormAdd.jsx
+++ src/components/FormAdd/FormAdd.jsx
@@ -4,13 +4,22 @@ import { increment } from '../store/caloreisSlice';
 
 const FormAdd = () => {
     const dispatch = useDispatch();
-    const [text, setText] = useState('');
+    const [countCcal, setCountCcal] = useState('');
+    const [countGram, setCountGram] = useState('');
+
+    const calculateCalories = countCcal / 100 * countGram;
 
     return (
         <div className="max-w-sm mx-auto">
-            <label htmlFor="input" className="mb-2 text-sm font-medium text-gray-900">How many calories did you eat today?</label>
-            <input className="input bg-gray-50 rounded-md border border-gray-300 ps-2 p-2.5 m-2" type="text" value={text} onChange={e => setText(e.target.value)}/>
-            <button className="text-white bg-blue-700 rounded-md px-5 py-2.5 text-center m-2" onClick={() => { dispatch(increment(text)); setText('');}}>Add</button>
+            <div>
+                <label htmlFor="input-ccal" className="mb-2 block text-sm font-medium text-gray-900">How many calories are in 100g?</label>
+                <input className="input-ccal bg-gray-50 rounded-md border border-gray-300 ps-2 p-2.5 m-2" type="text" value={countCcal} onChange={e => setCountCcal(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="input-gram" className="mb-2 block text-sm font-medium text-gray-900">How many grams?</label>
+                <input className="input-gram bg-gray-50 rounded-md border border-gray-300 ps-2 p-2.5 m-2" type="text" value={countGram} onChange={e => setCountGram(e.target.value)}/>
+            </div>
+            <button className="text-white bg-blue-700 rounded-md px-5 py-2.5 text-center m-2" onClick={() => { dispatch(increment(calculateCalories)); setCountCcal(''); setCountGram('');}}>Add</button>
         </div>
     );
 }
--- src/components/store/caloreisSlice.js
+++ src/components/store/caloreisSlice.js
@@ -3,14 +3,14 @@ import { createSlice } from '@reduxjs/toolkit';
 export const caloriesSlice = createSlice({
     name: 'calories',
     initialState: {
-        value: 0
+        value: 0.0
     },
     reducers: {
         increment: (state, action) => {
             const newValue = action.payload;
-            
-            if(newValue.length && !isNaN(newValue))
-                state.value += +action.payload;
+
+            if(!isNaN(newValue))
+                state.value += action.payload;
         },
         clear: state => {
             state.value = 0;
